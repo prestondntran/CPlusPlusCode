1f001f84c535ffe2d7457d62d52dd951eff6c652
prestondntran <86041345+prestondntran@users.noreply.github.com>
2021-06-20 01:44:35 -0700

    Create bmimetric.cpp

---
+++ bmimetric.cpp
@@ -0,0 +1,22 @@
+/* This program asks the user for metric height and weight and outputs the BMI with two decimal places. */
+
+#include <iostream>
+using namspace std;
+
+int main() {
+  float weight, height;
+  float BMI;
+  
+  cout<<"Please enter weight in kilograms: ";
+  cin>>weight;
+  cout<<"Please enter height in meters: ";
+  cin>>height;
+  
+  BMI = weight / (height * height)
+  BMI = int(BMI * 100)
+  BMI = BMI / 100
+    
+  cout<<"BMI is: "<<BMI;
+  
+  return 0;
+}
