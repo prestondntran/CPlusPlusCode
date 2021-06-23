1ca79945acd4407d7e851839668ac49de7ad9186
prestondntran <86041345+prestondntran@users.noreply.github.com>
2021-06-23 01:42:05 -0700

    Update isleapyearc.cpp

--- isleapyearc.cpp
+++ isleapyearc.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int user_year;
+int main(int argc) {
+  int year;
   
-  cin>>user_year;
+  cin>>year;
   
-  if ((user_year % 4 == 0 && user_year % 100 != 0) || (user_year % 400 == 0))
-    cout<<user_year<<" was a leap year"<<endl;
+  if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    cout<<year<<" was a leap year"<<endl;
   else
-    cout<<user_year<<" was not a leap year"<<endl;
+    cout<<year<<" was not a leap year"<<endl;
   
   return 0;
 }
