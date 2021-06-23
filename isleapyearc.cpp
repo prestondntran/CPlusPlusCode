371486585a05d24f1b74f035ae8ce53671a4116d
prestondntran <86041345+prestondntran@users.noreply.github.com>
2021-06-23 01:32:50 -0700

    Create isleapyearc.cpp

---
+++ isleapyearc.cpp
@@ -0,0 +1,17 @@
+/* This program asks the user for a year and determines whether the year is a leap year. */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int year;
+  
+  cin>>year;
+  
+  if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    cout<<year<<" was a leap year"<<endl;
+  else
+    cout<<year<<" was not a leap year"<<endl;
+  
+  return 0;
+}
